2d0bab804d72a7951df5362c23c9baa8bbbb4b48
Rajasekharan Vengalil <avranju@gmail.com>
2018-11-05 14:38:48 -0800

    Add k8s operator project

---
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.K8s.Operator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.Azure.Devices.Edge.Agent.K8s.Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
