f0437717adfe7ec50cd5cb7ebf034f510935e39a
Rajasekharan Vengalil <avranju@gmail.com>
2018-11-05 16:14:12 -0800

    wip

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.K8s.Operator/Program.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.K8s.Operator/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace Microsoft.Azure.Devices.Edge.Agent.K8s.Operator
 {
+    using System;
+    using System.IO;
+    using k8s;
+    using static System.Environment;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // load the k8s config from $HOME/.kube/config if its available
+            KubernetesClientConfiguration kubeConfig;
+            string kubeConfigPath = Path.Combine(Environment.GetFolderPath(SpecialFolder.UserProfile), ".kube", "config");
+            if (File.Exists(kubeConfigPath))
+            {
+                kubeConfig = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+            }
+            else
+            {
+                kubeConfig = KubernetesClientConfiguration.InClusterConfig();
+            }
+
+            var client = new Kubernetes(kubeConfig);
         }
     }
 }
